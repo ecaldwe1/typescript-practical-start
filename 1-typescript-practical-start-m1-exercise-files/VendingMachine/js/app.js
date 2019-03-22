f1e791294b62c7db640b68c78ca6d8d8d7fbfbb4
Beth.Caldwell <beth.caldwell@target.com>
2019-03-22 13:34:38 -0500

    Add exercise files

---
+++ 1-typescript-practical-start-m1-exercise-files/VendingMachine/js/app.js
@@ -0,0 +1,8 @@
+var vendingMachine = (function () {
+    function vendingMachine() {
+    }
+    return vendingMachine;
+}());
+/// <reference path="vendingMachine.ts" />
+var machine = new vendingMachine();
+//# sourceMappingURL=app.js.map
